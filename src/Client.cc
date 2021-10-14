28c1dd288bd9ed0a30f84e9ab247b551045f72c8
jdes01 <jasampa3000@gmail.com>
2021-10-14 10:14:08 +0200

    feat: Client class functions added; still need to be done; client class file headers stil need to be added

---
+++ src/Client.cc
@@ -0,0 +1,21 @@
+#include "Client.h"
+
+Client::Client(string serverIpAddress, int serverPortNumber){}
+
+void Client::openClientSocket_(){}
+
+void Client::fillServerSocketDataStructure_(string serverIpAddress, int serverPortNumber){}
+
+void Client::requestServerConnection_(){}
+
+void Client::setFileDescriptorStructures_(){}
+
+void Client::readServerMessage_(){}
+
+void Client::handleServerErrorMessages_(){}
+
+void Client::sendMessageToServer_(){}
+
+void Client::handleClientMessage_(){}
+
+void Client::startComunication(){}
---
+++ src/Client.h
@@ -0,0 +1,41 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <sys/types.h>
+
+// sockaddr_in
+#include <netinet/in.h>
+
+// close()
+#include <unistd.h>
+
+#include <string>
+using std::string;
+
+
+class Client {
+
+    private:
+        int clientSocketDescriptor_;
+        struct sockaddr_in serverSocketData_;   // tipo de direccion, puerto y direccion IP
+        fd_set readerFileDescriptor_;           // descriptor de readfds, que usara la funcion select
+        char messageBuffer_[128];       // buffer de la funcion recvfrom
+        bool endComunication_;
+
+        void openClientSocket_();
+        void fillServerSocketDataStructure_(string serverIpAddress, int serverPortNumber);
+        void requestServerConnection_();
+        void setFileDescriptorStructures_();
+
+        void readServerMessage_();
+        void handleServerErrorMessages_();
+        void sendMessageToServer_();
+        void handleClientMessage_();
+
+    public:
+        Client(string serverIpAddress, int serverPortNumber);
+        void startComunication();
+        inline void closeClient() { close(clientSocketDescriptor_); }
+};   
+
+#endif
